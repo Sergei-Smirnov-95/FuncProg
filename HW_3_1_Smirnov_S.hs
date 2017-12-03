61ef2efa0cf55f0363d0ef0dada0fa1dd69f4a1b
Sergei-Smirnov-95 <cec11@yandex.ru>
2017-12-03 18:01:44 +0300

    Исправление quotRem, добавление тестов к нему

--- HW_3_1_Smirnov_S.hs
+++ HW_3_1_Smirnov_S.hs
@@ -4,21 +4,33 @@ data WeirdPeanoNumber = Zero | Succ (WeirdPeanoNumber ) | Pred (WeirdPeanoNumber
 
 instance Eq WeirdPeanoNumber where
  (==) Zero Zero = True
- (==) (Succ a) (Succ b) = if a==b then True else False
- (==) (Pred a) (Pred b) = if a==b then True else False 
- (==) a b = False
+ (==) (Succ a) (Succ b) = a==b 
+ (==) (Pred a) (Pred b) = a==b  
+ (==) (Succ a) (Pred b) = a==(Pred(Pred b))
+ (==) (Pred a) (Succ b) = b==(Pred(Pred a))
+ (==) a Zero = False
+ (==) Zero a = False
 
 instance Ord WeirdPeanoNumber where
  (<=) Zero Zero = True
  (<=) Zero (Succ b) = True
  (<=) Zero (Pred b) = False
  
- (<=) (Pred a) (Pred b) = if a<=b then True else False
- (<=) (Succ a) (Succ b) = if a<=b then True else False
+ (<=) (Pred a) (Pred b) = a<=b 
+ (<=) (Succ a) (Succ b) =  a<=b 
  
  (<=) (Pred a) b = True
  (<=) (Succ a) b = False
  
+ (<) Zero Zero = False
+ (<) Zero (Succ b) = True
+ (<) Zero (Pred b) = False
+ 
+ (<) (Pred a) (Pred b) = a<b 
+ (<) (Succ a) (Succ b) =  a<b 
+ 
+ (<) (Pred a) b = True
+ (<) (Succ a) b = False
 instance Num WeirdPeanoNumber where
  (+) Zero b = b
  (+) a Zero = a
@@ -27,7 +39,10 @@ instance Num WeirdPeanoNumber where
  (+) (Pred a) (Succ b) = a + b
  (+) (Succ a) (Pred b) = a + b
  
- (-) Zero b = fromInteger(- toInteger b)
+  --(-) Zero b = fromInteger(- toInteger b)
+ (-) Zero Zero = Zero
+ (-) Zero (Succ a)= Pred(Zero - a)
+ (-) Zero (Pred a)= Succ(Zero - a) 
  (-) a Zero = a
  (-) (Succ a) (Succ b) = a - b
  (-) (Pred a) (Pred b) = a - b
@@ -82,10 +97,34 @@ instance Integral WeirdPeanoNumber where
  toInteger (Pred a) = toInteger a - 1
  toInteger (Succ a) = toInteger a + 1
   
- quotRem a b = (fromInteger $ (quot(toInteger a) (toInteger b)),fromInteger $ (rem(toInteger a) (toInteger b)))
+ quotRem a Zero = error "zero deriving"
+ quotRem a b | (a>Zero && b>Zero) =  helpquotRem a b
+             | (a<Zero && b<Zero) =  helpquotRem (Zero-a) (Zero-b)
+             | (a>Zero && b<Zero) =  (Zero - fst (helpquotRem a (Zero-b)), Zero - snd (helpquotRem a (Zero-b)))
+             | (a<Zero && b>Zero) =  (Zero - fst (helpquotRem (Zero-a) b), Zero - snd (helpquotRem (Zero-a) b))
+             | otherwise = (Zero,Zero)
+             where helpquotRem a b |(a==b)  = (Succ(Zero),Zero) 
+                                   |(a > b) = (Succ(fst (helpquotRem (a-b) b)),snd (helpquotRem (a-b) b))
+                                   |(a < b) = (Zero,a)
+
   
 instance Show WeirdPeanoNumber where
  show a = show(toInteger a )
  
 myPeanoTest = (Zero + fromInteger (-2))*(abs (toEnum (-3))) 
+
+a = Zero 
+b = (Pred a) 
+c = (Pred b) 
+d = (Succ a)
+e = (Succ d) 
+
+test1 = quotRem b c
+test2 = quotRem c b
+test3 = quotRem d c
+test4 = quotRem c d
+test5 = quotRem b e
+test6 = quotRem e d 
+test7 = quotRem b a
+test8 = quotRem a b
 --MyPeano
